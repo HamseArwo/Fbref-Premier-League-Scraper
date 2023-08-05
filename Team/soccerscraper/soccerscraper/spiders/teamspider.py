31de597ac83e2a804643a6bd9f53a3ba67e8a902
Hamse <arwohamseu@gmail.com>
2023-08-05 16:49:46 -0400

    Change spider code for better efficiency

--- Team/soccerscraper/soccerscraper/spiders/teamspider.py
+++ Team/soccerscraper/soccerscraper/spiders/teamspider.py
@@ -6,7 +6,7 @@ class TeamspiderSpider(scrapy.Spider):
     name = "teamspider"
     allowed_domains = ["fbref.com"]
     start_urls = [
-        "https://fbref.com/en/comps/9/Premier-League-Stats#all_results2022-202391"]
+        "https://fbref.com/en/comps/9/2022-2023/2022-2023-Premier-League-Stats"]
 
     def parse(self, response):
         table_rows = response.css("#results2022-202391_overall tbody tr")
@@ -14,13 +14,23 @@ class TeamspiderSpider(scrapy.Spider):
             column = row.css("td")
             clubitem = ClubItems()
             clubitem["rank"] = row.css("th::text").get()
+
+            items = ["match_played", "wins", "draws",
+                     "losses", "points", "goal_differences"]
+            indexs = [1, 2, 3, 4, 8, 7,]
+            for item, index in zip(items, indexs):
+
+                clubitem[item] = column[index].css("::text").get()
+
+                pass
+
             clubitem["team"] = column[0].css("a::text").get()
-            clubitem["match_played"] = column[1].css("::text").get()
-            clubitem["wins"] = column[2].css("::text").get()
-            clubitem["draws"] = column[3].css("::text").get()
-            clubitem["losses"] = column[4].css("::text").get()
-            clubitem["points"] = column[8].css("::text").get()
-            clubitem["goal_differences"] = column[7].css("::text").get()
+            # clubitem["match_played"] = column[1].css("::text").get()
+            # clubitem["wins"] = column[2].css("::text").get()
+            # clubitem["draws"] = column[3].css("::text").get()
+            # clubitem["losses"] = column[4].css("::text").get()
+            # clubitem["points"] = column[8].css("::text").get()
+            # clubitem["goal_differences"] = column[7].css("::text").get()
             clubitem["top_scorer"] = column[15].css("a::text").get()
             # yield clubitem
 
